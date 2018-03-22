d5a44e64ebe86ea789714fb5ba00c2f52e31b938
lexdenotter <lexdenotter@home.nl>
2018-03-22 15:19:34 +0100

    start Junit

---
+++ src/test/java/com/ElegantDevelopment/iacWebshop/JUNIT.java
@@ -0,0 +1,17 @@
+package com.ElegantDevelopment.iacWebshop;
+
+
+import com.ElegantDevelopment.iacWebshop.model.Product;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JUNIT {
+    @Test
+    public void ProductTest() {
+        Product tester = new Product(); // Product is tested
+
+        // assert statements
+        assertTrue(0, );
+    }
+}
